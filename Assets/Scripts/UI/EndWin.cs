780fa0faba7d4d47130fc40dc921d76c35e5fcaf
Inès D <ines.dakkak@edu.esiee.fr>
2023-09-21 13:37:45 +0200

    Niveau

---
+++ Assets/Scripts/UI/EndWin.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndWin : MonoBehaviour
+{
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    
+}
