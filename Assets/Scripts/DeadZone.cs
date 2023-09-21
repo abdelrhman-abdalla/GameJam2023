0bcc2724668711bf8900c2ffae3c05d88eed86dd
Gabriel <gabriel.rouleau@edu.esiee.fr>
2023-09-21 14:51:13 +0200

    Main level.

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerStats stats = collision.GetComponent<PlayerStats>();
+
+            if (stats != null)
+            {
+                stats.Kill();
+            }
+        }
+    }
+}
